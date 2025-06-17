94ff56de70244ba8ff46abf43a3f3b29c096870a
haniffal <04hanif10alfarisi02@gmail.com>
2025-06-17 21:15:52 +0700

    Update augmentations.py
    Signed-off-by: haniffal <04hanif10alfarisi02@gmail.com>

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -30,14 +30,16 @@ class Albumentations:
             check_version(A.__version__, "1.0.3", hard=True)  # version requirement
 
             T = [
-                A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
+                # A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
                 A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.0),
-                A.RandomGamma(p=0.0),
-                A.ImageCompression(quality_lower=75, p=0.0),
+                A.Rotate(limit=(-10,10), p=0.5),
+                A.HorizontalFlip(p=0.5),
+                # A.MedianBlur(p=0.01),
+                # A.ToGray(p=0.01),
+                # A.CLAHE(p=0.01),
+                # A.RandomBrightnessContrast(p=0.0),
+                # A.RandomGamma(p=0.0),
+                # A.ImageCompression(quality_lower=75, p=0.0),
             ]  # transforms
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
 
