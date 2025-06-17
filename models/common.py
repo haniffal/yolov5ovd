c63d8e0c70e4e03c98700d3134749dd66a6575db
haniffal <04hanif10alfarisi02@gmail.com>
2025-06-17 22:16:23 +0700

    Update common.py
    Signed-off-by: haniffal <04hanif10alfarisi02@gmail.com>

--- models/common.py
+++ models/common.py
@@ -82,10 +82,10 @@ class Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initializes a standard convolution layer with optional batch normalization and activation."""
         super().__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False).to(self.device)
+        self.bn = nn.BatchNorm2d(c2).to(self.device)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity().to(self.device)
     def forward(self, x):
         """Applies a convolution followed by batch normalization and an activation function to the input tensor `x`."""
         x = x.to(self.device)
